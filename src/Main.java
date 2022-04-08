4f4d2a1b317121bff53f3608a4cc1d1e64713c57
Foxssj <nicanorgonzalez2003@gmail.com>
2022-04-08 14:04:41 +0100

    Feature: añadidos los metodos load() y unLoad() a Cargo

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,58 @@
 import com.SpaceshipSim.SpaceShip;
+import com.SpaceshipSim.Cargo;
 
 import java.util.Scanner;
 
-public class Main {
+    public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Como se llama tu nave?");
-        String nombreNave = sc.nextLine();
+            System.out.println("Como se llama tu nave?");
+            String nombreNave = sc.nextLine();
 
-        System.out.println("Cual es la matrícula de tu nave?");
-        String matricGalactica = sc.nextLine();
+            System.out.println("Cual es la matrícula de tu nave?");
+            String matricGalactica = sc.nextLine();
 
-        System.out.println("Cual es la potencia de aceleración de tu nave?");
-        int aceleracion = sc.nextInt();
+            System.out.println("Cual es la potencia de aceleración de tu nave?");
+            int aceleracion = sc.nextInt();
 
-        System.out.println("En que coordenada se encuentra? (X)");
-        int coordenadaX = sc.nextInt();
-        sc.nextLine();
+            System.out.println("En que coordenada se encuentra? (X)");
+            int coordenadaX = sc.nextInt();
+            sc.nextLine();
 
-        SpaceShip nicanave = new SpaceShip(nombreNave, matricGalactica, aceleracion, coordenadaX);
+            SpaceShip nicanave = new SpaceShip(nombreNave, matricGalactica, aceleracion, coordenadaX);
 
-        System.out.println(nicanave + "\n" + "Que quiere hacer con su nave?");
+            System.out.println(nicanave + "\n" + "Que quiere hacer con su nave?");
 
 
-        boolean salir = false;
-        while (salir != true) {
-            String eleccion = sc.nextLine();
-            switch (eleccion) {
-                case "Acelerar":
-                case "acelerar":
-                    System.out.println(nicanave + "\n Las coordenadas de la nave serian ("
-                            + nicanave.speedUp() + ", 0)");
-                    System.out.println("\t--> Que quiere hacer con su nave?");
-                    break;
-                case "frenar":
-                case "Frenar":
-                    System.out.println(nicanave + "\n Las coordenadas de la nave serian ("
-                            + nicanave.brake() + ", 0)");
-                    System.out.println("\t-->Que quiere hacer con su nave?");
-                    break;
-                case "parar":
-                case "Parar":
-                    System.out.println(nicanave + "\n Las coordenadas de la nave serian ("
-                            + nicanave.emergencyStop() + ", 0)");
-                    System.out.println("\t-->Que quiere hacer con su nave?");
-                    break;
-                case "Salir":
-                case "salir":
-                    System.out.println("Abandonando la nave...");
-                    salir = true;
+            boolean salir = false;
+            while (salir != true) {
+                String eleccion = sc.nextLine();
+                switch (eleccion) {
+                    case "Acelerar":
+                    case "acelerar":
+                        System.out.println(nicanave + "\n Las coordenadas de la nave serian ("
+                                + nicanave.speedUp() + ", 0)");
+                        System.out.println("\t--> Que quiere hacer con su nave?");
+                        break;
+                    case "frenar":
+                    case "Frenar":
+                        System.out.println(nicanave + "\n Las coordenadas de la nave serian ("
+                                + nicanave.brake() + ", 0)");
+                        System.out.println("\t-->Que quiere hacer con su nave?");
+                        break;
+                    case "parar":
+                    case "Parar":
+                        System.out.println(nicanave + "\n Las coordenadas de la nave serian ("
+                                + nicanave.emergencyStop() + ", 0)");
+                        System.out.println("\t-->Que quiere hacer con su nave?");
+                        break;
+                    case "Salir":
+                    case "salir":
+                        System.out.println("Abandonando la nave...");
+                        salir = true;
+                }
             }
         }
-    }
 }
--- src/com/SpaceshipSim/Cargo.java
+++ src/com/SpaceshipSim/Cargo.java
@@ -1,32 +1,49 @@
-package com.SpaceshipSim;
+    package com.SpaceshipSim;
 
-public class Cargo extends SpaceShip {
+    public class Cargo extends SpaceShip {
 
-    private int maxCargo;
-    private int containers;
+        private int maxCargo;
+        private int containers;
 
-    public Cargo(String nombre, String matrículaGaláctica,
-                 int aceleracion, int coordenadaX, int maxCargo, int containers) {
-        super(nombre, matrículaGaláctica, aceleracion, coordenadaX);
-        this.maxCargo = maxCargo;
-        this.containers = containers;
-    }
+        public Cargo(String nombre, String matrículaGaláctica,
+                     int aceleracion, int coordenadaX, int maxCargo, int containers) {
+            super(nombre, matrículaGaláctica, aceleracion, coordenadaX);
+            this.maxCargo = maxCargo;
+            this.containers = containers;
+        }
 
-    public int getMaxCargo() {
-        return maxCargo;
-    }
+        public int getMaxCargo() {
+            return maxCargo;
+        }
 
-    public void setMaxCargo(int maxCargo) {
-        this.maxCargo = maxCargo;
-    }
+        public void setMaxCargo(int maxCargo) {
+            this.maxCargo = maxCargo;
+        }
 
-    public int getContainers() {
-        return containers;
-    }
+        public int getContainers() {
+            return containers;
+        }
 
-    public void setContainers(int containers) {
-        this.containers = containers;
-    }
+        public void setContainers(int containers) {
+            this.containers = containers;
+        }
 
+        public int load() {
+            if (containers >= maxCargo) {
+                System.out.println("La nave de carga ya está llena");
+            } else {
+                containers = containers + 1;
+            }
+            return containers;
+        }
+
+        public int unLoad() {
+            if (containers <= 0) {
+                System.out.println("La nave de carga está vacia");
+            } else {
+                containers = containers - 1;
+            }
+            return containers;
+        }
 
 }
